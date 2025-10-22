7a3d29abe35d49918d4dfe938428d0655345c1a9
Mikerah <mikerah@Mikerahs-MacBook-Air.local>
2025-10-22 01:00:03 -0400

    Basic round management

--- src/StoffelCoordinator.sol
+++ src/StoffelCoordinator.sol
@@ -2,8 +2,42 @@ pragma solidity ^0.8.13;
 
 import "./StoffelAccessControl.sol";
 import "./StoffelInputManager.sol";
+import "./IStoffelAccessControl.sol";
+import "./IStoffelInputManager.sol";
 
 contract StoffelCoordinator is StoffelAccessControl, StoffelInputManager {
+    enum Rounds {
+        PreprocessingRound,
+        ClientInputMaskReservationRound,
+        CollectingClientInputRound,
+        ClientInputsCollectionEndRound
+        MPCTaskExecutionRound,
+        ClientOutputCollectionRound
+    }
 
+    bytes32 _stoffelLangProgramHash;
+    uint256 public creationTime = block.timestamp;
+
+    Rounds round = Rounds.PreprocessingRound;
+
+    modifier atRound(Rounds _round) {
+        require(_round == round);
+        _;
+    }
+
+    function nextRound() internal {
+        round = Rounds(uint(round) + 1);
+    }
+
+    modifier timedRoundTransition(Rounds transitionRound, uint whenToTransition) {
+        if (round == transitionRound && (block.timestamp >= creationTime + whenToTransition)) {
+            nextRound();
+        }
+
+    }
+
+    function initCoordinator() {}
+
+    
 
 }
