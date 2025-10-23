3455cc2f1bf0f0eb8d5b1ef674de285966f98b0b
Mikerah <mikerah@stoffelmpc.com>
2025-10-23 01:47:32 -0400

    Added base function signatures for abstract StoffelCoordinator contract with some comments

--- src/StoffelCoordinator.sol
+++ src/StoffelCoordinator.sol
@@ -5,7 +5,7 @@ import "./StoffelInputManager.sol";
 import "./IStoffelAccessControl.sol";
 import "./IStoffelInputManager.sol";
 
-contract StoffelCoordinator is StoffelAccessControl, StoffelInputManager {
+abstract contract StoffelCoordinator is StoffelAccessControl, StoffelInputManager {
     enum Rounds {
         PreprocessingRound,
         ClientInputMaskReservationRound,
@@ -15,6 +15,23 @@ contract StoffelCoordinator is StoffelAccessControl, StoffelInputManager {
         MPCTaskExecutionEndRound,
         ClientOutputCollectionRound
     }
+
+    struct Inputs {
+        bytes publicInputs;
+        MaskedInput[] maskedInputs;
+    }
+
+    struct Outputs {
+        bytes publicOutputs;
+        mapping(address => mapping (address => bool)) sharesReceived;
+    }
+
+    mapping (address => Inputs) clientInputs;
+
+
+    event CoordinatorInitialized(address coordinator, uint timeofInitialization, address designatedParty);
+    event PreprocessingRoundEnded();
+    event MPCTaskExecuted(bytes32 stoffelProgramHash, address executor, uint timeOfExecution);
 
     bytes32 _stoffelLangProgramHash;
     uint256 public creationTime = block.timestamp;
@@ -49,11 +66,41 @@ contract StoffelCoordinator is StoffelAccessControl, StoffelInputManager {
         _;
     }
 
-    function initCoordinator();
+    /* Need to initiate the following
+      - Set designated party
+      - Set MPC program hash
+      - grant party role to initial set of MPC nodes
+    */
+    function initCoordinator() {
+        
+    };
 
-    function initiateMPCComputation();
+    /**
+     * Preprocessing can be started by the designated party 
+     * Depending on the trust model of the contract that instantiates this abstract contract
+     * the designated party can set the input mask indices or each party can contribute to setting
+     * the input mask indices.
+     */
+    function startPreprocessing() atRound(Rounds.PreprocessingRound);
 
-    function publishPublicOutput();
+    /**
+     * Once the indices have been set, clients should now be able to reserve an index
+     * for an input mask that they can request for off-chain from the MPC nodes. 
+     * Once reserved, the clients can now post their public and masked inputs to the resulting app.
+     */
+    function gatherInputs();
+
+    /**
+     * Once enough inputs have been collected according to the application's logic, the MPC computation should be initiated
+     */
+    function initiateMPCComputation() atRound(Rounds.ClientInputsCollectionEndRound);
+
+    /**
+     * Once the computation has been completed by the MPC nodes off-chain
+     * The resulting public outputs are posted on-chain and the shares are sent directly to the client for them to reconstruct.
+     * The Outputs struct keeps track of whether a particular party has sent the final share back to a client
+     */
+    function publishOutputs();
 
     
     
