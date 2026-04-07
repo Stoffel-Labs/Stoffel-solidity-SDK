e8f3dad12ff448045617ab484a23c8e1e408d103
cadaniluk <cordian@stoffelmpc.com>
2026-04-07 16:43:44 +0200

    make test work

--- test/StoffelAccessControl.t.sol
+++ test/StoffelAccessControl.t.sol
@@ -10,7 +10,7 @@ contract StoffelAccessControlTest is Test {
     function setUp() public {
 	uint256 t = 1;
     	address[] memory initialMpcNodes = new address[](5);
-	initialMpcNodes[0] = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+	initialMpcNodes[0] = address(this);
 	initialMpcNodes[1] = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8;
 	initialMpcNodes[2] = 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC;
 	initialMpcNodes[3] = 0x90F79bf6EB2c4f870365E785982E1f101E93b906;
